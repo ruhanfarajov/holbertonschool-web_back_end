7f47cc107d6d88fba792267b4ed4e9c9994d1af1
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-30 13:04:01 -0500

    smdsl;kad

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Write a Python function that lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """mongo_collection will be the pymongo collection object
+       Return an empty list if no document in the collection
+    """
+    return list(mongo_collection.find())
